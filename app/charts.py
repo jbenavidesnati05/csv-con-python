e2398090ef8b60411e38e57eb6f84e471b67fed1
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-04-15 19:49:40 -0500

    first commit

---
+++ app/charts.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt 
+
+def generate_bar_char(labels, values):
+    fig, ax = plt.subplots()
+    ax.bar(labels, values)
+    plt.show()
+
+def generate_pie_char(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels = labels)
+    ax.axis('equal')
+    plt.show()
+
+    
+if __name__ == '__main__':
+    labels = ['a','b','c']
+    values = [100,200,300]
+    # generate_bar_char( labels , values)
+    generate_pie_char( labels , values)
